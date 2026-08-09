ba2c1fab5546af665c9f1012a31fbcb0a8516f94
NiloyAch <niloyacharjee677@gmail.com>
2026-08-09 09:08:15 +0600

    Class-6 homeworks

---
+++ TwitterClone.Domain/Entities/Like.cs
@@ -0,0 +1,26 @@
+﻿
+namespace TwitterClone.Domain.Entities
+{
+    public class Like
+    {
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _likedAt;
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime LikedAt
+        {
+            get { return _likedAt; }
+        }
+    }
+
+}
---
+++ TwitterClone.Domain/Entities/Tweet.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Tweet
+    {
+        private Guid _userId;
+        private Guid _tweetId;
+        private string _content;
+        public Guid Id 
+        { 
+            get { return _userId; } 
+        }
+
+        public Guid AuthorId
+        {
+            get { return _tweetId; }
+        }
+
+        public string Content
+        {  get { return _content; } 
+
+            set { _content = value; }
+        }
+    }
+}
