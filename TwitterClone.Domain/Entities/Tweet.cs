51a977bc8fcb710f4ca5f6c6845eff2091dfc0aa
NiloyAch <niloyacharjee677@gmail.com>
2026-08-14 15:39:33 +0600

    fix domain entity inheritance

--- TwitterClone.Domain/Entities/Tweet.cs
+++ TwitterClone.Domain/Entities/Tweet.cs
@@ -1,63 +1,33 @@
-﻿
-using System.Reflection.Metadata;
-using TwitterClone.Domain.Entities;
+﻿using TwitterClone.Domain.Entities;
 
 namespace TwitterClone.Domain.Entities
 {
-    public class Tweet
     public class Tweet : BaseEntity
     {
-        private Guid _id;
         private Guid _userId;
         private string _content;
-        private DateTime _createdAt;
-        private DateTime _modifiedAt;
-        private Guid _createdBy;
-        private Guid _modifiedBy;
 
-
-        public Tweet()
         public Tweet(string content) : base(Guid.NewGuid())
         {
-            _id = Guid.NewGuid();
-            _createdAt = DateTime.UtcNow;
-        }
-
-        public Guid Id
-        {
-            get { return _id; }
             _content = content;
         }
 
         public Guid UserId
-            set { _content = value; }
-}
-
-        public DateTime CreatedAt
-                public override string DescribeRecord()
         {
-            get { return _createdAt; }
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        public override string DescribeRecord()
+        {
             var baseRecord = base.DescribeRecord();
             return $"{baseRecord}, UserId: {UserId}, Content: {Content}";
         }
-
-        public DateTime ModifiedAt
-        {
-            get { return _modifiedAt; }
-            set { _modifiedAt = value; }
-        }
-
-        public Guid CreatedBy
-        {
-            get { return _createdBy; }
-            set { _createdBy = value; }
-        }
-
-        public Guid ModifiedBy
-        {
-            get { return _modifiedBy; }
-            set { _modifiedBy = value; }
-        }
-
     }
 }
