e7064f43d67ab766dee42ca45a63c34496737dac
NiloyAch <niloyacharjee677@gmail.com>
2026-08-14 15:52:43 +0600

    Fix domain entity inheritance

--- TwitterClone.Domain/Entities/Retweet.cs
+++ TwitterClone.Domain/Entities/Retweet.cs
@@ -1,61 +1,39 @@
-﻿using System.Xml.Linq;
-using TwitterClone.Domain.Entities;
+﻿using TwitterClone.Domain.Entities;
 
 namespace TwitterClone.Domain.Entities
 {
-    public class Retweet
     public class Retweet : BaseEntity
     {
-        private Guid _id;
         private Guid _userId;
         private Guid _tweetId;
         private string _comment;
-        private DateTime _createdAt;
-        private DateTime _modifiedAt;
-        private Guid _createdBy;
-        private Guid _modifiedBy;
 
-        public Retweet()
         public Retweet() : base(Guid.NewGuid())
         {
-            _id = Guid.NewGuid();
-            _createdAt = DateTime.UtcNow;
-        }
-
-        public Guid Id
-        {
-            get { return _id; }
         }
 
         public Guid UserId
-            set { _comment = value; }
-}
-
-    public DateTime CreatedAt
-    {
-        get { return _createdAt; }
-    }
-
-    public DateTime ModifiedAt
-    {
-        get { return _modifiedAt; }
-        set { _modifiedAt = value; }
-    }
-
-    public Guid CreatedBy
-    {
-        get { return _createdBy; }
-        set { _createdBy = value; }
-    }
-
-    public Guid ModifiedBy
-            public override string DescribeRecord()
-    {
-        get { return _modifiedBy; }
-        set { _modifiedBy = value; }
-        var baseRecord = base.DescribeRecord();
-        return $"{baseRecord}, UserId: {UserId}, TweetId: {TweetId}, Comment: {Comment}";
-    }
-
+        {
+            get { return _userId; }
+            set { _userId = value; }
         }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+            set { _tweetId = value; }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set { _comment = value; }
+        }
+
+        public override string DescribeRecord()
+        {
+            var baseRecord = base.DescribeRecord();
+            return $"{baseRecord}, UserId: {UserId}, TweetId: {TweetId}, Comment: {Comment}";
+        }
+    }
 }
